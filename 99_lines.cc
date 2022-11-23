e16f15308f538820f8e2b89acced9029ae40dd03
whoslcy <whoslcy@foxmail.com>
2022-11-23 19:30:23 +0800

    --allow-empty

--- 99_lines.cc
+++ 99_lines.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstddef>
 #include <iomanip>
 #include <vector>
 #include <cmath>
@@ -8,64 +9,61 @@
 
 using std::setprecision;
 using std::setw;
+using std::min;
+using std::max;
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::VectorXi;
+using Eigen::ArrayXi;
 
 int main()
 {
-    int nelx = 80;
-    int nely = 40;
-    double volfrac = 0.5;
-    double penal = 3;
-    double rmin =  2;
-    // TODO(whoslcy@foxmail.com): 和 Matrix<double, nely, nelx> 相比哪个更好
-    MatrixXd x(nely, nelx);
-    // MatrixXd 默认是列优先存储的，按照列的顺序赋值会更快一些
-    // TODO(whoslcy@foxmail.com): 引用矩阵的元素要求用什么类型的值？
-    for (size_t i = 0; i < nelx; i++)
-    {
-        for (size_t j = 0; j < nely; j++)
-        {
-            x(j, i) = volfrac;
-        }
-    }
-    // TODO(whoslcy@foxmail.com): size_t 跟 string::size_type 是一回事吗
+    size_t nelx = 80;
+    size_t nely = 40;
+    double  volfrac = 0.5,
+            penal = 3,
+            rmin =  2;
+    MatrixXd x(nely, nelx) = MatrixXd::Constant(nely, nelx, volfrac);
     size_t loop = 0;
     double change = 1.0;
     while (change > 0.01){
         ++loop;
         MatrixXd xold(nely, nelx) = x;
-
-        // FE-ANALYSIS
-
         // ELEMENT STIFFNESS MATRIX
         double E = 1.0;
         double nu = 0.3;
-        VectorXd k(8) = {0/2-nu/6, 1/8+nu/8, -1/4-nu/12, -1/8+3*nu/8, -1/4+nu/12, -1/8-nu/8, nu/6, 1/8-3*nu/8};
-        MatrixXd KE(8, 8) = E/(1-nu*nu) * {
-            {k(1), k(2), k(3), k(4), k(5), k(6), k(7), k(8)},
-            {k(2), k(1), k(8), k(7), k(6), k(5), k(4), k(3)},
-            {k(3), k(8), k(1), k(6), k(7), k(4), k(5), k(2)},
-            {k(4), k(7), k(6), k(1), k(8), k(3), k(2), k(5)},
-            {k(5), k(6), k(7), k(8), k(1), k(2), k(3), k(4)},
-            {k(6), k(5), k(4), k(3), k(2), k(1), k(8), k(7)},
-            {k(7), k(4), k(5), k(2), k(3), k(8), k(1), k(6)},
-            {k(8), k(3), k(2), k(5), k(4), k(7), k(6), k(1)},
+        // 这里向量的维度之所以设置为 9 是因为下面 KE 的那一大块是直接复制过来的
+        // 但是因为 VectorXd 的下标是从 0 开始的，所以需要在 k(0) 处占用一个位置
+        VectorXd k(9) = {
+            0,
+            0.5-nu/6, 0.125+nu/8, -0.25-nu/12, -0.125+3*nu/8,
+            -0.25+nu/12, -0.125-nu/8, nu/6, 0.125-3*nu/8
         };
-
-        MatrixXd K(2*(nelx+1)*(nely+1), 2*(nelx+1)*(nely+1)),
-                 F(2*(nelx+1)*(nely+1)),
-                 U(2*(nely+1)*(nelx+1));
+        MatrixXd KE(8, 8) {
+            {k(1), k(2), k(3), k(4), k(5), k(6), k(7), k(8)}
+            {k(2), k(1), k(8), k(7), k(6), k(5), k(4), k(3)}
+            {k(3), k(8), k(1), k(6), k(7), k(4), k(5), k(2)}
+            {k(4), k(7), k(6), k(1), k(8), k(3), k(2), k(5)}
+            {k(5), k(6), k(7), k(8), k(1), k(2), k(3), k(4)}
+            {k(6), k(5), k(4), k(3), k(2), k(1), k(8), k(7)}
+            {k(7), k(4), k(5), k(2), k(3), k(8), k(1), k(6)}
+            {k(8), k(3), k(2), k(5), k(4), k(7), k(6), k(1)}
+        };
+        KE *= E/(1-nu*nu);
+        // FE-ANALYSIS
+        MatrixXd K(2*(nelx+1)*(nely+1), 2*(nelx+1)*(nely+1));
+        VectorXd F(2*(nelx+1)*(nely+1)),
+                 U(2*(nely+1)*(nelx+1)) = VectorXd::Zero(2*(nely+1)*(nelx+1));
         for (int elx = 1; elx <= nelx; ++elx)
         {
             for (int ely = 1; ely <= nely; ++ely)
             {
                 int n1 = (nely+1)*(elx-1) + ely;
                 int n2 = (nely+1)*elx + ely;
-                std::vector<int> edof {2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
+                ArrayXi edof {2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
                 // MATLAB 里的矩阵和向量是以 1 为起始下标的，而 Eigen 里的矩阵都是以 0 为起始下标的，所以这里的 edof 相比 MATLAB 里的都要少 1
+                edof -= 1;
                 for (auto &value : edof)
                 {
                     value--;
@@ -94,7 +92,9 @@ int main()
         }
         U(free_dofs) = K(free_dofs, free_dofs).colPivHouseholderQr().solve(F(free_dofs));
         U(fixed_dofs) = 0;
+
         double c = 0;
+        MatrixXd dc(nely, nelx);
         for (int elx = 1; elx <= nelx; ++elx)
         {
             for (int ely = 1; ely <= nely; ++ely)
@@ -102,12 +102,56 @@ int main()
                 int n1 = (nely+1)*(elx-1) + ely;
                 int n2 = (nely+1)*elx + ely;
                 std::vector<int> indexes {2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
-                VectorXi Ue(8) = U(indexes);
                 // MATLAB 里的矩阵和向量是以 1 为起始下标的，而 Eigen 里的矩阵都是以 0 为起始下标的，所以这里的 edof 相比 temp 里的都要少 1
-                c = c + Ue.transpose()*KE*Ue*pow(x(ely-1, elx-1), penal);
-
+                for (auto &index : indexes)
+                {
+                    index -= 1;
+                }
+                VectorXi Ue(8) = U(indexes);
+                int index_ely = ely-1,
+                    index_elx = elx-1;
+                c = c + Ue.transpose()*KE*Ue*pow(x(index_ely,index_elx), penal);
+                dc(index_ely, index_elx) = -penal*pow(x(index_ely, index_elx), (penal-1))*Ue.transpose()*KE*Ue
             }
         }
+
+        // FILTERING OF SENSITIVITIES
+        MatrixXd dcn(nely, nelx) = MatrixXd::Zero(nely, nelx);
+        for (int i = 1; i <= nelx; i++)
+        {
+            for (int j = 1; j <= nely; j++)
+            {
+                double sum = 0.0;
+                for (int k = std::max(i-std::floor(rmin), 1); k <= std::min(i+std::floor(rmin), nelx); k++)
+                {
+                    for (int l = std::max(j-floor(rmin), 1); l <= std::min(j+std::floor(rmin), nely); l++)
+                    {
+                        double fac = rmin-std::sqrt((i-k)*(i-k)+(j-l)*(j-l));
+                        sum += std::max(0, fac);
+                        int j_index = j-1,
+                            i_index = i-1;
+                        dcn(j_index, i_index) += std::max(0, fac)*x(l-1, k-1)*dc(l-1, k-1);
+                    }
+                }
+            }
+        }
+        
+        // DESIGN UPDATE BY THE OPTIMALITY CRITERIA METHOD
+        double  l1 = 0,
+                l2 = 100000,
+                move = 0.2;
+        MatrixXd xnew(nely, nelx);
+        while (l2-l1 > 1e-4)
+        {
+            double lmid = 0.5*(l2+l1);
+            xnew = max(0.001, max(x-move, min(1.0, min(x+move, x.cwiseProduct(std::sqrt(-dc/lmid))))));
+            if (xnew.sum() - volfrac*nelx*nely > 0){
+                l1 = lmid;
+            }else {
+                l2 = lmid;
+            }
+        }
+        x = xnew;
         // TODO(whoslcy@foxmail.com): 下面这个能行得通吗 cwiseAbs() 的返回值是一个 矩阵吗
         change = (x - x_old).cwiseAbs().maxCoeff();
         std::cout
